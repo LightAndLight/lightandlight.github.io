0ce8b4cda2672ea96dd5df0ab78af62a0bbd1c1e
Isaac Elliott <isaace71295@gmail.com>
2025-08-04 20:13:35 +1000

    publish

--- docs/res/script.js
+++
@@ -1,8 +0,0 @@
-function navColor(bool) {
-  var nav = document.getElementsByTagName("nav")
-  if (bool) {
-    nav[0].style.borderColor = "#4f86c6";
-  } else {
-    nav[0].style.borderColor = "#2f2f2f";
-  }
-}
